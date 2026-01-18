a9e5abc053c7ba483b5079b147f9e856d3f999e1
FlowniX <92480881+sfwx@users.noreply.github.com>
2026-01-18 15:51:32 -0300

    Atualizar o script.js

--- c/script.js
+++ c/script.js
@@ -1,1 +1,21 @@
-.
+document.getElementById("year").textContent = new Date().getFullYear();
+function tagFromName(name) {
+  let sum = 0;
+  name = name.toLowerCase();
+  for (let c of name) sum += c.charCodeAt(0);
+  return String(sum % 10000).padStart(4, "0");
+}
+function convertText(text) {
+  if (!text) {
+    document.getElementById("result").textContent = "#0000";
+    history.replaceState(null, "", "#");
+    return;
+  }
+  const tag = tagFromName(text);
+  document.getElementById("result").textContent = "#" + tag;
+  location.hash = tag;
+}
+// se abrir com hash na URL
+if (location.hash.length === 5) {
+  document.getElementById("result").textContent = "#" + location.hash.substring(1);
+}
