7a89bc65533ae654a5c835afa65f9ec873de36b4
FlowniX <92480881+sfwx@users.noreply.github.com>
2026-01-17 22:31:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const actions = {
   "0626": {
     "id": "0626",
     "enabled": true,
-    "type": "external_link",
+    "type": "navigate",
     "image": "https://sfwx.github.io/index1.png",
     "title": "FwX Perfil pessoal",
     "description": "Ainda incompleto!!",
@@ -62,12 +62,12 @@ const actions = {
   "1631": {
     "id": "1631",
     "enabled": true,
-    "type": "file_download",
+    "type": "navigate",
     "image": "https://sfwx.github.io/signature.png",
     "title": "Arco de Operador",
     "description": "Clique para baixar o arquivo do incrível Arco de Operador!",
     "value": "mcstructure/bow.mcstructure",
-    "target": "_self",
+    "target": "_blank",
     "confirm": {
       "enabled": true,
       "message": "Baixar Arquivo"
@@ -76,12 +76,12 @@ const actions = {
   "2176": {
     "id": "2176",
     "enabled": true,
-    "type": "file_download",
+    "type": "navigate",
     "image": "https://sfwx.github.io/signature.png",
     "title": "Fogos de Artifício",
     "description": "Clique para baixar o arquivo de Fogos de Artifício de Operador!",
     "value": "mcstructure/firework.mcstructure",
-    "target": "_self",
+    "target": "_blank",
     "confirm": {
       "enabled": true,
       "message": "Baixar Arquivo"
@@ -90,12 +90,12 @@ const actions = {
   "1936": {
     "id": "1936",
     "enabled": true,
-    "type": "file_download",
+    "type": "navigate",
     "image": "https://sfwx.github.io/signature.png",
     "title": "Escudo de Operador",
     "description": "Clique para baixar o arquivo do incrível Escudo de Operador!",
     "value": "mcstructure/shield.mcstructure",
-    "target": "_self",
+    "target": "_blank",
     "confirm": {
       "enabled": true,
       "message": "Baixar Arquivo"
@@ -104,12 +104,12 @@ const actions = {
   "1944": {
     "id": "1944",
     "enabled": true,
-    "type": "file_download",
+    "type": "navigate",
     "image": "https://sfwx.github.io/signature.png",
     "title": "Asas de Operador",
     "description": "Clique para baixar o arquivo das incríveis Asas de Operador!",
     "value": "mcstructure/elytra.mcstructure",
-    "target": "_self",
+    "target": "_blank",
     "confirm": {
       "enabled": true,
       "message": "Baixar Arquivo"
@@ -118,12 +118,12 @@ const actions = {
   "1044": {
     "id": "1044",
     "enabled": true,
-    "type": "file_download",
+    "type": "navigate",
     "image": "https://sfwx.github.io/signature.png",
     "title": "Give Camera",
     "description": "Clique para baixar o arquivo do Item de Câmera.",
     "value": "mcstructure/camera.mcstructure",
-    "target": "_self",
+    "target": "_blank",
     "confirm": {
       "enabled": true,
       "message": "Baixar Arquivo"
@@ -132,12 +132,12 @@ const actions = {
   "1731": {
     "id": "1731",
     "enabled": true,
-    "type": "file_download",
+    "type": "navigate",
     "image": "https://sfwx.github.io/signature.png",
     "title": "Give Nether Portal",
     "description": "Clique para baixar o arquivo do Bloco de Portal do Nether.",
     "value": "mcstructure/nether_portal.mcstructure",
-    "target": "_self",
+    "target": "_blank",
     "confirm": {
       "enabled": true,
       "message": "Baixar Arquivo"
@@ -146,12 +146,12 @@ const actions = {
   "1396": {
     "id": "1396",
     "enabled": true,
-    "type": "file_download",
+    "type": "navigate",
     "image": "https://sfwx.github.io/signature.png",
     "title": "Give End Portal",
     "description": "Clique para baixar o arquivo do Bloco de Portal do End.",
     "value": "mcstructure/end_portal.mcstructure",
-    "target": "_self",
+    "target": "_blank",
     "confirm": {
       "enabled": true,
       "message": "Baixar Arquivo"
@@ -160,12 +160,12 @@ const actions = {
   "1611": {
     "id": "1611",
     "enabled": true,
-    "type": "file_download",
+    "type": "navigate",
     "image": "https://sfwx.github.io/signature.png",
     "title": "Give Ender Portal",
     "description": "Clique para baixar o arquivo do Bloco de Portal do Ender.",
     "value": "mcstructure/ender_portal.mcstructure",
-    "target": "_self",
+    "target": "_blank",
     "confirm": {
       "enabled": true,
       "message": "Baixar Arquivo"
@@ -197,42 +197,17 @@ function fwxInput(event, input) {
 
 // ===== ACTION HANDLER =====
 function fwxRedirect(action) {
-  switch (action.type) {
-    case "navigate":
-      if (action.target == "_self") {
-        window.location.href = 'https://sfwx.github.io/' + action.value;
-      }
-      else if (action.target == "_blank") {
-        window.open('https://sfwx.github.io/' + action.value, "_blank");
-      }
-    break;
-    case "external_link":
-      if (action.target == "_self") {
-        window.location.href = action.value;
-      }
-      else if (action.target == "_blank") {
-        window.open(action.value, "_blank");
-      }
-    break;
-    case "file_download":
-      const a = document.createElement("a");
-      a.href = action.value;
-      a.download = "";
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-    break;
-    case "download_redirect":
-      if (action.target == "_self") {
-        window.location.href = action.value;
-      }
-      else if (action.target == "_blank") {
-        window.open(action.value, "_blank");
-      }
-    break;
-    default:
-      console.warn("Tipo desconhecido:", action.type);
-    break;
+  if (action.type == "navigate" && action.target == "_self") {
+    window.location.href = 'https://sfwx.github.io/' + action.value;
+  }
+  else if (action.type == "navigate" && action.target == "_blank") {
+    window.open('https://sfwx.github.io/' + action.value, "_blank");
+  }
+  else if (action.type == "external" && action.target == "_self") {
+    window.location.href = action.value;
+  }
+  else if (action.type == "external" && action.target == "_blank") {
+    window.open(action.value, "_blank");
   }
 }
 
