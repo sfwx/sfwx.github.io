414cfa369774ebb91ca079804d568da08faa298c
FlowniX <92480881+sfwx@users.noreply.github.com>
2026-02-08 19:01:13 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ console.log("FwX: Olá mundo!");
   if (path.endsWith(".html")) {
     const newPath = path.replace(/\.html$/, "");
     window.location.replace(newPath + window.location.search + window.location.hash);
-  } else if (pach == "index") {
-    window.location.replace("/");
   }
   document.querySelectorAll("*").forEach(element => {
     setTimeout(() => {
