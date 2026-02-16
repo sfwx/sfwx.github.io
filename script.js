c8d1ea761341c963c1407ea06ddb586e6a6e9dec
FlowniX <92480881+sfwx@users.noreply.github.com>
2026-02-15 21:45:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// ===== INPUT =====
+window.addEventListener("load", () => {
+  const hash = window.location.hash;
+  if (hash) {
+    const value = hash.substring(1);
+    if (/^\d+$/.test(value)) {
+      const corrected = value.padStart(4, "0");
+      window.location.href = corrected;
+    }
+  }
+});
 function fwxInput(event, input) {
   if (event.key === "Enter" && input.value.trim() !== "") {
-    window.location.href = /^\d+$/.test(input.value) ? input.value.padStart(4, '0') : input.value;
+    window.location.href = /^\d+$/.test(input.value) ? input.value.padStart(4, "0") : input.value;
     input.value = "";
   }
 }
-
-// ===== EVENTS =====
-window.addEventListener("load", {
-  window.location.href = window.location.hash.substring(1).toLowerCase().padStart(4, '0')
-});
