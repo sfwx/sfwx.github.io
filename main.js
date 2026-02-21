ac7969f62551e3b447a646ce6cbb8cd1a6739511
FlowniX <92480881+sfwx@users.noreply.github.com>
2026-02-21 00:05:05 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 /* https://sfwx.github.io/copyright */
 
 window.fwx = {
-  version: [ 4, 0, 4 ].join("."),
+  version: "4.0.4",
   license: {
-    text: ["©", new Date().getFullYear(), "FlowniX. Todos os Direitos são Reservados."].join(" "),
+    text: "© FlowniX. Todos os Direitos são Reservados.",
     url: "https://sfwx.github.io/copyright",
     year() {
       if (document.getElementById("fwxCopyrightYear")) {
@@ -16,25 +16,29 @@ window.fwx = {
   log(text) {
     console.log("FwX:", text);
   }
+  extension(active = false) {
+    if (!active) {
+      const path = window.location.pathname;
+      if (path.endsWith("/index.html")) {
+        const newPath = path.replace(/\/index.html$/, "/");
+        window.location.replace(newPath + window.location.search + window.location.hash);
+      }
+      else if (path.endsWith(".html")) {
+        const newPath = path.replace(/\.html$/, "");
+        window.location.replace(newPath + window.location.search + window.location.hash);
+      }
+      else if (path.endsWith("/index")) {
+        const newPath = path.replace(/\/index$/, "/");
+        window.location.replace(newPath + window.location.search + window.location.hash);
+      }
+    }
+  }
 };
+fwx.extension(false);
 fwx.log("Olá mundo!");
 
 (function () {
-  if (true) {
-    const path = window.location.pathname;
-    if (path.endsWith("/index.html")) {
-      const newPath = path.replace(/\/index.html$/, "/");
-      window.location.replace(newPath + window.location.search + window.location.hash);
-    }
-    else if (path.endsWith(".html")) {
-      const newPath = path.replace(/\.html$/, "");
-      window.location.replace(newPath + window.location.search + window.location.hash);
-    }
-    else if (path.endsWith("/index")) {
-      const newPath = path.replace(/\/index$/, "/");
-      window.location.replace(newPath + window.location.search + window.location.hash);
-    }
-  }
+  fwx.year();
   document.querySelectorAll("*:not([data-fwx])").forEach(element => {
     setTimeout(() => {
       element.setAttribute("data-fwx", "");
