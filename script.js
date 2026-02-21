8666462169513d43abb344aee045e1a5e5008a4c
FlowniX <92480881+sfwx@users.noreply.github.com>
2026-02-21 00:15:47 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -2,16 +2,14 @@
 /* Todos os direitos são reservados */
 /* https://sfwx.github.io/copyright */
 
-window.addEventListener("load", () => {
-  const hash = window.location.hash;
-  if (hash) {
-    const value = hash.substring(1);
-    if (/^\d+$/.test(value)) {
-      const corrected = value.padStart(4, "0");
-      window.location.href = corrected;
-    }
+const fwx.hash = window.location.hash.substring(1);
+if (fwx.hash) {
+  if (/^\d+$/.test(fwx.hash)) {
+    const corrected = fwx.hash.padStart(4, "0");
+    window.location.href = corrected;
   }
-});
+}
+
 function fwxInput(event, input) {
   if (event.key === "Enter" && input.value.trim() !== "") {
     window.location.href = /^\d+$/.test(input.value) ? input.value.padStart(4, "0") : input.value;
