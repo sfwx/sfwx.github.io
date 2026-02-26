303291093517b5882f2e1691ee9df7e9cc938f00
FlowniX <92480881+sfwx@users.noreply.github.com>
2026-02-25 23:21:51 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -17,44 +17,36 @@ window.fwx = {
     }
   },
   log(type, text, popup = false, scroll = true) {
-  const types = {
-    fwx: { label: "FwX:", color: "violet" },
-    info: { label: "INFO:", color: "lightskyblue" },
-    success: { label: "SUCESSO:", color: "lightgreen" },
-    warn: { label: "AVISO:", color: "palegoldenrod" },
-    error: { label: "ERRO:", color: "lightcoral" }
-  };
-
-  if (!types[type]) return;
-
-  const { label, color } = types[type];
-
-  // Console colorido
-  if (type === "warn") {
-    console.warn(`%c${label} ${text}`, `color: ${color};`);
-  } else if (type === "error") {
-    console.error(`%c${label} ${text}`, `color: ${color};`);
-  } else {
-    console.log(`%c${label} ${text}`, `color: ${color};`);
-  }
-
-  // Console customizado na página
-  const fwxConsole = document.getElementById("fwxConsole");
-  if (fwxConsole) {
-    const span = document.createElement("span");
-    span.setAttribute("data-fwx", "");
-    span.classList.add(type);
-    span.textContent = `${label} ${text}`;
-
-    fwxConsole.appendChild(span);
-
-    if (scroll) {
-      fwxConsole.scrollTop = fwxConsole.scrollHeight;
+    const types = {
+      fwx: { label: "FwX:", color: "violet" },
+      info: { label: "INFO:", color: "lightskyblue" },
+      success: { label: "SUCESSO:", color: "lightgreen" },
+      warn: { label: "AVISO:", color: "palegoldenrod" },
+      error: { label: "ERRO:", color: "lightcoral" }
+    };
+    if (!types[type]||!text) return;
+    const { label, color } = types[type];
+    if (type === "warn") {
+      console.warn(`%c${label} ${text}`, `color: ${color};`);
+    } else if (type === "error") {
+      console.error(`%c${label} ${text}`, `color: ${color};`);
+    } else {
+      console.log(`%c${label} ${text}`, `color: ${color};`);
     }
-  }
-
-  if (popup) alert(text);
-},
+    // Console customizado na página
+    const fwxConsole = document.getElementById("fwxConsole");
+    if (fwxConsole) {
+      const span = document.createElement("span");
+      span.setAttribute("data-fwx", "");
+      span.classList.add(type);
+      span.textContent = `${label} ${text}`;
+      fwxConsole.appendChild(span);
+      if (scroll) {
+        fwxConsole.scrollTop = fwxConsole.scrollHeight;
+      }
+    }
+    if (popup) alert(text);
+  },
   extension(active = false) {
     if (!active) {
       const path = window.location.pathname;
